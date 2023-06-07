d77af4e76bff83f004e981dcee236fac76f07d62
askkrishnaprasad <120196234+askkrishnaprasad@users.noreply.github.com>
2023-06-07 11:01:22 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  project     = "blissful-flame-388621"
+  region      = "us-central1"
+  credentials = file("/home/jenkins/terraform-gcp/blissful-flame-388621-5a7858ffa9ef.json")
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+  }
+}
