edb2f959696c9302f2b51053891677225ca1ac01
askkrishnaprasad <120196234+askkrishnaprasad@users.noreply.github.com>
2023-06-08 10:27:34 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "The project ID to host resources in"
+  type        = string
+}
+
+variable "credentials" {
+  description = "The credentials JSON file path"
+  type        = string
+}
